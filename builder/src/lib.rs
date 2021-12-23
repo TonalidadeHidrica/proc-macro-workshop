d34c727cb95d472194c3e47ab7557eea9acb7129
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2021-12-24 02:09:39 +0900

    WIP: workshop

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,75 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
+use quote::quote;
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let name = input.ident;
+    let builder_name = Ident::new(&format!("{}Builder", name), Span::call_site());
+
+    let str = match input.data {
+        Data::Struct(s) => s,
+        Data::Enum(_) | Data::Union(_) => panic!("Enums and unions are not supported"),
+    };
+    let fields = match str.fields {
+        Fields::Named(f) => f,
+        Fields::Unnamed(_) | Fields::Unit => panic!("Tuple structs and units are not supported"),
+    };
+    let constructor = fields.named.iter().map(|x| {
+        let ident = &x.ident;
+        quote!( #ident: None )
+    });
+    let fields_option = fields.named.iter().map(|x| {
+        let ident = &x.ident;
+        let ty = &x.ty;
+        quote!( #ident: Option<#ty> )
+    });
+    let setters = fields.named.iter().map(|x| {
+        let ident = &x.ident;
+        let ty = &x.ty;
+        quote! {
+            fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                self.#ident = Some(#ident);
+                self
+            }
+        }
+    });
+    let build_fields = fields.named.iter().map(|x| {
+        let ident = &x.ident;
+        quote! {
+            #ident: self.#ident.as_ref()?.clone()  // :thinking_face:
+        }
+    });
+
+    let ret = quote! {
+        impl #name {
+            fn builder() -> #builder_name {
+                #builder_name {
+                    #(#constructor),*
+                }
+            }
+        }
+
+        struct #builder_name {
+            #(#fields_option),*
+        }
+
+        impl #builder_name {
+            #(#setters)*
+
+            fn build(&mut self) -> anyhow::Result<#name> {
+                (|| -> Option<_> {
+                    Some(#name {
+                        #(#build_fields),*
+                    })
+                })().ok_or(|| anyhow!("Error!"))
+            }
+        }
+    };
+
+    // TokenStream::new()
+    ret.into()
 }
