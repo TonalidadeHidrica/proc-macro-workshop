0aad57429df064e33e6d3b88ec6eccb1f2502c4d
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2021-12-26 16:11:31 +0900

    Completed

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,16 +1,19 @@
-use itertools::Itertools;
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
 use syn::{
-    parse_macro_input, parse_str, Data, DeriveInput, Field, Fields, GenericArgument, Ident, Lit,
-    Meta, NestedMeta, Path, PathArguments, Type,
+    parse_macro_input, parse_str, spanned::Spanned, Data, DeriveInput, Field, Fields,
+    GenericArgument, Ident, Lit, Meta, NestedMeta, Path, PathArguments, Type,
 };
 
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-
+    derive_impl(input)
+        .unwrap_or_else(syn::Error::into_compile_error)
+        .into()
+}
+fn derive_impl(input: DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     let name = input.ident;
     let builder_name = Ident::new(&format!("{}Builder", name), Span::call_site());
 
@@ -22,31 +25,35 @@ pub fn derive(input: TokenStream) -> TokenStream {
         Fields::Named(f) => f,
         Fields::Unnamed(_) | Fields::Unit => panic!("Tuple structs and units are not supported"),
     };
-    let option_types = fields
-        .named
-        .iter()
-        .map(|x| match as_surrounding_type(&x.ty, "Option") {
+    let option_types = fields.named.iter().map(|x| {
+        syn::Result::Ok(match as_surrounding_type(&x.ty, "Option") {
             Some(ty) => ClassifyType::Option(ty),
             None => match check_attr(x) {
-                Some(res) => res,
+                Some(res) => res?,
                 None => ClassifyType::Other(&x.ty),
             },
-        });
-    let fields = fields.named.iter().zip(option_types).collect_vec();
+        })
+    });
+    let fields = fields
+        .named
+        .iter()
+        .zip(option_types)
+        .map(|(x, y)| syn::Result::Ok((x, y?)))
+        .collect::<Result<Vec<_>, _>>()?;
     let fields_option = fields.iter().map(|(x, opt)| {
         let ident = &x.ident;
         let ty = &x.ty;
         match opt {
-            ClassifyType::Vec(ty, _) => quote!( #ident: Vec<#ty> ),
-            _ => quote!( #ident: Option<#ty> ),
+            ClassifyType::Vec(ty, _) => quote!( #ident: ::std::vec::Vec<#ty> ),
+            _ => quote!( #ident: ::std::option::Option<#ty> ),
         }
     });
     let constructor = fields.iter().map(|(x, opt)| {
         let ident = &x.ident;
         match opt {
-            ClassifyType::Option(_) => quote!( #ident: Some(None) ),
-            ClassifyType::Vec(..) => quote!( #ident: Vec::new() ),
-            ClassifyType::Other(_) => quote!( #ident: None ),
+            ClassifyType::Option(_) => quote!( #ident: ::std::option::Option::Some(::std::option::Option::None) ),
+            ClassifyType::Vec(..) => quote!( #ident: ::std::vec::Vec::new() ),
+            ClassifyType::Other(_) => quote!( #ident: ::std::option::Option::None ),
         }
     });
     let setters = fields.iter().map(|(x, opt)| {
@@ -55,7 +62,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
             ClassifyType::Option(ty) => {
                 quote! {
                     fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                        self.#ident = Some(Some(#ident));
+                        self.#ident = ::std::option::Option::Some(::std::option::Option::Some(#ident));
                         self
                     }
                 }
@@ -63,7 +70,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
             ClassifyType::Other(ty) => {
                 quote! {
                     fn #ident(&mut self, #ident: #ty) -> &mut Self {
-                        self.#ident = Some(#ident);
+                        self.#ident = ::std::option::Option::Some(#ident);
                         self
                     }
                 }
@@ -108,8 +115,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
             #(#setters)*
 
             fn build(&mut self) -> ::anyhow::Result<#name> {
-                (|| -> Option<_> {
-                    Some(#name {
+                (|| -> ::std::option::Option<_> {
+                    ::std::option::Option::Some(#name {
                         #(#build_fields),*
                     })
                 })().ok_or_else(|| ::anyhow::anyhow!("Error!"))
@@ -117,8 +124,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     };
 
-    // TokenStream::new()
-    ret.into()
+    // Ok(ret.into())
+    Ok(ret)
 }
 
 enum ClassifyType<'a> {
@@ -156,7 +163,7 @@ fn as_surrounding_type<'a, 'b>(ty: &'a Type, name: &'b str) -> Option<&'a Type> 
     }
 }
 
-fn check_attr(field: &Field) -> Option<ClassifyType> {
+fn check_attr(field: &Field) -> Option<syn::Result<ClassifyType>> {
     let attr = field
         .attrs
         .iter()
@@ -171,13 +178,18 @@ fn check_attr(field: &Field) -> Option<ClassifyType> {
     }
     let val = match val.first()? {
         NestedMeta::Meta(Meta::NameValue(v)) if path_is_just(&v.path, "each") => v,
-        _ => return None,
+        _ => {
+            return Some(Err(syn::Error::new(
+                val.span(),
+                r#"expected `builder(each = "...")`"#,
+            )))
+        }
     };
     match &val.lit {
         Lit::Str(s) => {
             let ty = as_surrounding_type(&field.ty, "Vec")?;
             let ident = parse_str::<Ident>(&s.value()).ok()?;
-            Some(ClassifyType::Vec(ty, ident))
+            Some(Ok(ClassifyType::Vec(ty, ident)))
         }
         _ => None,
     }
